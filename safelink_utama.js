c7293d6a9667adcdf683f96c027441f08abaad8a
willyginanjar <52019604+willyginanjar@users.noreply.github.com>
2019-06-20 09:50:51 +0700

    Add files via upload

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="making-money-online-with-forex-trading_16.html"}
 if(x=="2"){xxx="how-to-become-successful-self-made.html"}
 if(x=="3"){xxx="making-money-online-with-forex-trading.html"}
-if(x=="4"){xxx="how-to-choose-best-health-insurance.html"}a_to_ve[i].href="https://thebestinssurance.blogspot.com/2018/12"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="how-to-choose-best-health-insurance.html"}a_to_ve[i].href="https://thebestinssurance.blogspot.com/2018/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
